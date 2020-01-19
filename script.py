991d90284436cde6f5aefba197d9d0fbb9ebb8a1
admin <pandiota@omonditedica.localdomain>
2020-01-19 21:55:55 +0600

    v0.1 OpenCV support

--- script.py
+++ script.py
@@ -1,22 +1,26 @@
+import cv2
 import numpy
 import face_recognition
 
+video_capture = cv2.VideoCapture(0)
 known_faces = []
-input_image = input()
 
-faces_image = face_recognition.load_image_file(input_image)
-
-bbox = face_recognition.face_locations(faces_image)
-desc = face_recognition.face_encodings(faces_image, known_face_locations=bbox)
-for i in desc:
-	dist = face_recognition.face_distance(known_faces, i)
-	if known_faces == []:
-		known_faces.append(i)
-		print("Welcome, User#"+str(len(known_faces)-1))
-		continue
-	kk = numpy.argmin(dist)
-	if dist[kk] < 0.6:
-		print("Hello again, User#"+str(kk))
-	else:
-		known_faces.append(i)
-		print("Welcome, User#"+str(len(known_faces)-1))
+while True:
+	_, frame_image = video_capture.read()
+	frame_image = frame_image[:, :, ::-1]
+	faces_image = face_recognition.load_image_file(frame_image)
+	bbox = face_recognition.face_locations(faces_image)
+	desc = face_recognition.face_encodings(faces_image, known_face_locations=bbox)
+	for i in desc:
+		dist = face_recognition.face_distance(known_faces, i)
+		if known_faces == []:
+			known_faces.append(i)
+			print("Welcome, User#"+str(len(known_faces)-1))
+			continue
+		kk = numpy.argmin(dist)
+		if dist[kk] < 0.6:
+			print("Hello again, User#"+str(kk))
+		else:
+			known_faces.append(i)
+			print("Welcome, User#"+str(len(known_faces)-1))
+video_capture.release()
